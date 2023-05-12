5d114eb58899f7db0259a7236974c547f02974fc
Pedro Souza <pedrocontato1840@gmail.com>
2023-05-11 23:45:27 -0300

    html-css

---
+++ recipes/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return render(request, 'recipes/pages/home.html')
+
+def contato(request):
+     return render(request, 'recipes/contato.html')
+
+def sobre(request):
+    return HttpResponse('SOBRE')
