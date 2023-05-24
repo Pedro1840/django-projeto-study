1d46680a1db61ceceeb703f65c32faaedcfb8de2
Pedro Souza <pedrocontato1840@gmail.com>
2023-05-24 00:09:01 -0300

    models 1

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Recipe
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
--- recipes/urls.py
+++ recipes/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 
 from . import views
 
+# * A variavel "app_name" tem que receber o 
+# nome em aspas simples * deixando recipe:(name path)
+app_name = 'recipes'
 
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipe),
+    path('', views.home, name="home"),
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
